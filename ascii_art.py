3920e046005dbb1d21e1ab0ff94cf226e0cd2af6
roy <endvroy@gmail.com>
2017-02-10 13:14:34 -0800

    added parallel convert, but slower

--- ascii_art.py
+++ ascii_art.py
@@ -1,8 +1,7 @@
 from PIL import Image
 import numpy as np
 import math, os
-
-scale = 0.43
+from concurrent import futures
 
 
 def translate_block(img):
@@ -12,38 +11,76 @@ def translate_block(img):
     return char_table[int(gray_scale / 255 * (len(char_table) - 1))]
 
 
-# def get_block_size(image, columns=80, scale=1):
+def convert_block(image, start_x, start_y, end_x, end_y):
+    block = image.crop((start_x, start_y, end_x, end_y))
+    return translate_block(block)
+
+
+# def convert_row(image, row, columns, block_width, block_height):
 #     width, height = image.size
-#     block_width = width / columns
-#     block_height = block_width / scale
-#     rows = height / block_height
-#     return math.ceil(rows)
+#     for column in range(columns):
+#         start_x, start_y = column * block_width, row * block_height
+#         end_x = int(start_x + block_width if start_x + block_width < width else width)
+#         end_y = int(start_y + block_height if start_y + block_height < height else height)
+#         start_x, start_y = int(start_x), int(start_y)
 
-
-def main():
-    file_path = input('input full file path: ')
-    base_name, *_ = os.path.splitext(file_path)
-    output_file_path = base_name + '.txt'
-    columns = int(input('input number of columns: '))
-
-    with Image.open(file_path).convert('L') as image: # image = image.convert(mode='L')
-        width, height = image.size
-        block_width = width / columns
-        block_height = block_width / scale
-        rows = math.ceil(height / block_height)
-        art = []
+def parallel_convert(image, columns, scale):
+    width, height = image.size
+    block_width = width / columns
+    block_height = block_width / scale
+    rows = math.ceil(height / block_height)
+    art = [[None for i in range(columns)] for i in range(rows)]
+    fd = {}
+    with futures.ProcessPoolExecutor(max_workers=4) as executor:
         for row in range(rows):
-            art.append('')
             for column in range(columns):
                 start_x, start_y = column * block_width, row * block_height
                 end_x = int(start_x + block_width if start_x + block_width < width else width)
                 end_y = int(start_y + block_height if start_y + block_height < height else height)
                 start_x, start_y = int(start_x), int(start_y)
-                block = image.crop((start_x, start_y, end_x, end_y))
-                art[row] += translate_block(block)
+                future = executor.submit(convert_block, image, start_x, start_y, end_x, end_y)
+                fd[row, column] = future
+                results = futures.as_completed(fd.values())
+                for k, v in fd.items():
+                    art[k[0]][k[1]] = v.result()
+
+    for row in range(rows):
+        art[row] = ''.join(art[row])
+
+    return '\n'.join(art)
+
+
+def sequential_convert(image, columns, scale):
+    width, height = image.size
+    block_width = width / columns
+    block_height = block_width / scale
+    rows = math.ceil(height / block_height)
+    art = []
+    for row in range(rows):
+        art.append('')
+        for column in range(columns):
+            start_x, start_y = column * block_width, row * block_height
+            end_x = int(start_x + block_width if start_x + block_width < width else width)
+            end_y = int(start_y + block_height if start_y + block_height < height else height)
+            start_x, start_y = int(start_x), int(start_y)
+            block = image.crop((start_x, start_y, end_x, end_y))
+            art[row] += translate_block(block)
+
+    return '\n'.join(art)
+
+
+def main():
+    scale = 0.43
+    file_path = input('input full file path: ')
+    base_name, *_ = os.path.splitext(file_path)
+    output_file_path = base_name + '.txt'
+    columns = int(input('input number of columns: '))
+
+    with Image.open(file_path).convert('L') as image:  # image = image.convert(mode='L')
+        art = sequential_convert(image, columns, scale)
 
     with open(output_file_path, 'w') as output_file:
-        output_file.write('\n'.join(art))
+        output_file.write(art)
         print('output written to {}'.format(output_file_path))
 
 
